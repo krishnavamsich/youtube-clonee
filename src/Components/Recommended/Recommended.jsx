b5ff85a3a5f4b861a4cca7a1377b96fb9e6d116f
Chebrolu.krishna <ch.krishnavamsi216@gmail.com>
2024-11-09 17:43:30 +0530

    [add] url to the feed

---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,38 @@
+import React,{useEffect, useState} from 'react'
+import './Recommended.css'
+import { API_KEY, value_converter } from '../../data'
+import { Link } from 'react-router-dom'
+
+const Recommended = ({categoryId}) => {
+
+  const [apiData,setApiData]=useState([])
+
+  const fetchData= async () =>{
+       const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=45&videoCategoryId=${categoryId}&key=${API_KEY}`
+       await fetch(relatedVideo_url).then(res=>res.json()).then(data=>setApiData(data.items));
+
+  }
+  useEffect(()=>{
+      fetchData();
+  },[])
+  return (
+    <div className="recommended">
+      {apiData.map((items,index)=>{
+          return(
+            <Link to={`/video/${items.snippet.categoryId}/${items.id}`}key={index} className="side-video-list">
+            <img src={items.snippet.thumbnails.medium.url} alt="" />
+            <div className="vid-info">
+                <h4>{items.snippet.title}</h4>
+                <p>{items.snippet.channelTitle}</p>
+                <p>{value_converter(items.statistics.viewCount)} views</p>
+            </div>
+          </Link>
+          )
+      })}
+      
+      
+    </div>
+  )
+}
+
+export default Recommended
