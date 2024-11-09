a2f0cc9dff382f4fc65268fd96a66468b10e1e0c
Chebrolu.krishna <ch.krishnavamsi216@gmail.com>
2024-11-09 10:04:48 +0530

    [add]images in sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -48,6 +48,24 @@ const Sidebar = () => {
         </div>
         <hr />
       </div>
+      <div className="subscribed-list">
+        <h3>Subscribed</h3>
+        <div className="side-link">
+          <img src={jack} alt="" /><p>PewDiePie</p>
+        </div>
+        <div className="side-link">
+          <img src={simon} alt="" /><p>Mr Beast</p>
+        </div>
+        <div className="side-link">
+          <img src={tom} alt="" /><p>Justin Bieber</p>
+        </div>
+        <div className="side-link">
+          <img src={jack} alt="" /><p>5-minute crafts</p>
+        </div>
+        <div className="side-link">
+          <img src={cameron} alt="" /><p>Nas Daily</p>
+        </div>
+      </div>
     </div>
   )
 }
